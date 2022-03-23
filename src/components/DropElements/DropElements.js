255d22fba1833ac12df6ce5dc7cf18d4da06a0be
Sourab Hossain <sourabhossain.fci@gmail.com>
2022-03-23 16:38:34 +0600

    drop elements components

---
+++ src/components/DropElements/DropElements.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DropElements.css';
+
+function DropElements() {
+  return (
+    <div className="DropElements"> 
+      Drop Elements Here    
+    </div>
+  );
+}
+
+export default DropElements;
